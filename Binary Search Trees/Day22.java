ce9cf1d8056c16c204487f4a69b63ab0759a9c85
Pinar Koroglu <33103674+pinarkoroglu@users.noreply.github.com>
2019-04-18 11:34:09 +0200

    Update Day22.java

--- Binary Search Trees/Day22.java
+++ Binary Search Trees/Day22.java
@@ -37,7 +37,7 @@ class Solution{
             return root;
         }
     }
-     public static void main(String args[]){
+    public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
         int T=sc.nextInt();
         Node root=null;
