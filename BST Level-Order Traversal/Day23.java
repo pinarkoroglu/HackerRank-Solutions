50dea0aefc3ec7647f25958e9d60174ce8a83c8d
Pinar Koroglu <33103674+pinarkoroglu@users.noreply.github.com>
2019-04-18 14:50:13 +0200

    Create Day23.java
    Day23-Java

---
+++ BST Level-Order Traversal/Day23.java
@@ -0,0 +1,53 @@
+import java.util.*;
+import java.io.*;
+class Node{
+    Node left,right;
+    int data;
+    Node(int data){
+        this.data=data;
+        left=right=null;
+    }
+}
+class Solution{
+
+static void levelOrder(Node root){
+      Queue<Node> queue = new LinkedList<>();
+      queue.add(root);
+      while(!queue.isEmpty()){
+        Node newNode= queue.remove();
+        System.out.print(newNode.data + " ");
+        if(newNode.left!=null)
+            queue.add(newNode.left);       
+        if(newNode.right!=null)
+            queue.add(newNode.right);
+    }
+    }
+
+public static Node insert(Node root,int data){
+        if(root==null){
+            return new Node(data);
+        }
+        else{
+            Node cur;
+            if(data<=root.data){
+                cur=insert(root.left,data);
+                root.left=cur;
+            }
+            else{
+                cur=insert(root.right,data);
+                root.right=cur;
+            }
+            return root;
+        }
+    }
+    public static void main(String args[]){
+            Scanner sc=new Scanner(System.in);
+            int T=sc.nextInt();
+            Node root=null;
+            while(T-->0){
+                int data=sc.nextInt();
+                root=insert(root,data);
+            }
+            levelOrder(root);
+        }	
+}
