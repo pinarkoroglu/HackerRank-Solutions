663d0cdfe08614c79e75eacf2e6986f19dfe76d4
Pinar Koroglu <33103674+pinarkoroglu@users.noreply.github.com>
2019-05-17 00:39:05 +0300

    Create Day28.java
    Day28-Java

---
+++ RegEx, Patterns, and Intro to Databases/Day28.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+         ArrayList<String> names = new ArrayList<>();
+         for (int NItr = 0; NItr < N; NItr++) {
+            String[] firstNameEmailID = scanner.nextLine().split(" ");
+
+            String firstName = firstNameEmailID[0];
+
+            String emailID = firstNameEmailID[1];
+
+            if(emailID.contains("@gmail.com")){
+                names.add(firstName);
+            }
+        }
+        Collections.sort(names);
+       for(String str:names){
+           System.out.println(str);
+       }
+        scanner.close();
+    }
+}
